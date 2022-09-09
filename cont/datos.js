d74b13003e4e0a0266d87ae8039007e45b06a2b1
jgranados4 <jgranados@est.ups.edu.ec>
2022-09-08 23:07:10 -0500

    fix: :bug: Cambios

--- cont/datos.js
+++ cont/datos.js
@@ -2,8 +2,10 @@ const Button = document.getElementById('Ingresar');
 let local = JSON.parse(localStorage.getItem('datos'));
 
 Button.addEventListener('click',()=>{
-  if(local[0].Password==1234){
-    location='/html/Plan.html'
+  if (local[0].User == local[0].User && local[0].Password == 1234) {
+    location = "/html/Plan.html";
+  }else{
+    alert('no es el usuario')
   }
 })
 console.log(local[0].Password);
